1bd430459c3f5e5ca869240ee5789a6c599552f1
JosueMartinez <jmartinez@cdevteam.com>
2020-07-17 16:45:56 -0400

    login image

--- Rodriguez.Mobile/Rodriguez.Mobile.Android/obj/Debug/90/android/environment.x86.s
+++ Rodriguez.Mobile/Rodriguez.Mobile.Android/obj/Debug/90/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"13483dbc-b9cf-4796-a987-a677fcec4bd8"
+	.asciz	"85a9063e-fa0f-48d4-a7a8-035ce05319b1"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
